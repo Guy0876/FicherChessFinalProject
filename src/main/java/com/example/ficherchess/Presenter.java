1922502276e45f03b7151f59b41640d4a9d8fdd7
Guy Moxham-Chozev <foxy07.08.06@gmail.com>
2025-01-26 10:42:08 +0200

    Update Presenter.java

--- src/main/java/com/example/ficherchess/Presenter.java
+++ src/main/java/com/example/ficherchess/Presenter.java
@@ -1,14 +1,18 @@
 package com.example.ficherchess;
 
 public class Presenter {
-
-    private Model model;
     private IView view;
 
+    public Presenter(IView view) {
+        this.view = view;
+    }
 
-    public Presenter(IView controller) {
-        this.model = new Model();
-        this.view = controller;
+    public void handlePieceSelection(int row, int col) {
+        // Handle piece selection logic
+    }
+
+    public void handlePieceMove(int oldRow, int oldCol, int newRow, int newCol) {
+        // Handle piece move logic
+        view.movePiece(oldRow, oldCol, newRow, newCol);
     }
 }
-
