16f00150068ad693645d31a4b5993cb903ac255f
Guy Moxham-Chozev <foxy07.08.06@gmail.com>
2025-01-27 15:21:03 +0200

    Update Bishops.java

--- src/main/java/com/example/ficherchess/Pieces/Bishops.java
+++ src/main/java/com/example/ficherchess/Pieces/Bishops.java
@@ -1,14 +1,13 @@
 package com.example.ficherchess.Pieces;
 
 public class Bishops extends Piece{
-    public Bishops(long bitboard) {
-        super(bitboard);
+    public Bishops(long bitboard, boolean isWhite) {
+        super(bitboard, isWhite);
     }
 
     @Override
     public long possibleMoves(long position){
         long specificBishop = bitboard & position;
-
         return diagonalMoves(specificBishop);
     }
 }
