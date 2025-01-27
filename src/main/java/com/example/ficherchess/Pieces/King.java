3a11424c5cdd46253795cbcd8f949ed94716fdfa
Guy Moxham-Chozev <foxy07.08.06@gmail.com>
2025-01-27 15:21:35 +0200

    Update King.java

--- src/main/java/com/example/ficherchess/Pieces/King.java
+++ src/main/java/com/example/ficherchess/Pieces/King.java
@@ -2,40 +2,38 @@ package com.example.ficherchess.Pieces;
 
 public class King extends Piece {
 
-    public King(long bitboard) {
-        super(bitboard);
+    public King(long bitboard, boolean isWhite) {
+        super(bitboard, isWhite);
     }
 
     @Override
     public long possibleMoves(long position){
         long specificKing = bitboard & position;
-        if(Piece.check){
-            return 0L;
-        }
+        long myPieces = isWhite ? whitePieces : blackPieces;
 
         // Move up
-        long up = specificKing << 8;
+        long up = specificKing << 8 & ~myPieces;
 
         // Move down
-        long down = specificKing >> 8;
+        long down = specificKing >> 8 & ~myPieces;
 
         // Move left
-        long left = specificKing << 1;
+        long left = specificKing << 1 & ~myPieces;
 
         // Move right
-        long right = specificKing >> 1;
+        long right = specificKing >> 1 & ~myPieces;
 
         // Move up-right
-        long upRight = specificKing << 7;
+        long upRight = specificKing << 7 & ~myPieces;
 
         // Move up-left
-        long upLeft = specificKing << 9;
+        long upLeft = specificKing << 9 & ~myPieces;
 
         // Move down-right
-        long downRight = specificKing >> 9;
+        long downRight = specificKing >> 9 & ~myPieces;
 
         //Move down-left
-        long downLeft = specificKing >> 7;
+        long downLeft = specificKing >> 7 & ~myPieces;
 
         return (up | down | left | right | upRight | upLeft | downRight | downLeft);
     }
